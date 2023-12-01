7f54794750770019bac377daaa77625505131843
gfi406 <makafanasiev@gmail.com>
2023-12-01 05:10:34 +0300

    upgrade main

--- Main.java
+++ Main.java
@@ -1,5 +1,46 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] agrs){
         System.out.println("это ветка main");
     }
+    import java.util.Scanner;
+
+    public class Main {
+        public static void main(String[] agrs){
+            System.out.println("это ветка dev");
+
+        }
+
+        public class Calculator {
+            public static void main(String[] args) {
+                Scanner scanner = new Scanner(System.in);
+
+                System.out.print("Enter first number: ");
+                double num1 = scanner.nextDouble();
+
+                System.out.print("Enter second number: ");
+                double num2 = scanner.nextDouble();
+
+                System.out.print("Enter operation (+ or -): ");
+                String operation = scanner.next();
+
+                double result;
+
+                if (operation.equals("+")) {
+                    result = num1 + num2;
+                }
+                else if (operation.equals("-")) {
+                    result = num1 - num2;
+                }
+                else {
+                    System.out.println("Unsupported operation!");
+                    return;
+                }
+
+                System.out.println("Result: " + result);
+            }
+        }
+    }
+
 }
